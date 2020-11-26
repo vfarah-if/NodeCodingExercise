4809230c0526345c239643a90e893b2d41d0dfce
vfarah-if <vincent.farah@codurance.com>
2020-11-26 11:08:40 +0000

    Small refactor

--- test-express/server/routes/users.js
+++ test-express/server/routes/users.js
@@ -1,49 +1,11 @@
 import { Router } from 'express';
 const router = Router();
-const userResponse =
-  [
-    {
-      user: {
-        name: 'Jane',
-        lastname: 'Doe'
-      }
-    },
-    {
-      user: {
-        name: 'Jon',
-        lastname: 'Doe'
-      }
-    },
-    {
-      user: {
-        name: 'Michael',
-        lastname: 'Caine'
-      }
-    },
-    {
-      user: {
-        name: 'James',
-        lastname: 'Bond'
-      }
-    },
-    {
-      user: {
-        name: 'Robert',
-        lastname: 'Plant'
-      }
-    },
-    {
-      user: {
-        name: 'Jordan',
-        lastname: 'Peterson'
-      }
-    },
-  ];
-
+import { getUserList } from '../users'
 
 router.get('/', function (req, res, next) {
   // console.debug(req, next);
-  res.json(userResponse);
+  const usersResponse = getUserList();
+  res.json(usersResponse);
 });
 
 export default router;
---
+++ test-express/server/users/index.js
@@ -0,0 +1,40 @@
+export const getUserList = () =>  {
+    return  [
+        {
+          user: {
+            name: 'Jane',
+            lastname: 'Doe'
+          }
+        },
+        {
+          user: {
+            name: 'Jon',
+            lastname: 'Doe'
+          }
+        },
+        {
+          user: {
+            name: 'Michael',
+            lastname: 'Caine'
+          }
+        },
+        {
+          user: {
+            name: 'James',
+            lastname: 'Bond'
+          }
+        },
+        {
+          user: {
+            name: 'Robert',
+            lastname: 'Plant'
+          }
+        },
+        {
+          user: {
+            name: 'Jordan',
+            lastname: 'Peterson'
+          }
+        },
+      ];
+}
