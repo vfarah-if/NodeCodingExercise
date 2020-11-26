f0d15e6b4555ae29d92b58c46a1fad0e3700609a
vfarah-if <vincent.farah@codurance.com>
2020-11-26 10:38:21 +0000

    Refactored tests to remove DRY Added test route out of the babel output folder

--- test-express/server/routes/index.test.js
+++ test-express/server/routes/index.test.js
@@ -1,10 +1,8 @@
-import app from "../app";
-import supertest from "supertest";
-const request = supertest(app);
+import { getTest } from "../../test-utilities/test-route";
 
 describe("index or home page", () => {
   it("should get the route", async (done) => {
-    const actualResponse = await request.get("/");
+    const actualResponse = await getTest("/");
     expect(actualResponse).toBeTruthy();
     expect(actualResponse.status).toBe(200);
     expect(actualResponse.text).toMatchSnapshot();
--- test-express/server/routes/users.test.js
+++ test-express/server/routes/users.test.js
@@ -1,10 +1,8 @@
-import app from "../app";
-import supertest from "supertest";
-const request = supertest(app);
+import { getTest } from "../../test-utilities/test-route";
 
 describe("users api", () => {
   it("should get the users json response", async (done) => {
-    const actualResponse = await request.get("/users");
+    const actualResponse = await getTest("/users");
     expect(actualResponse).toBeTruthy();
     expect(actualResponse.status).toBe(200);
     expect(actualResponse.text).toMatchSnapshot();
