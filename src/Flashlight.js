9ba8d0c1766868accb40cb84138c65908ea27d70
vfarah-if <vincent.farah@codurance.com>
2020-12-02 20:19:38 +0000

    CHeck in last and outstanding

--- src/Flashlight.js
+++ src/Flashlight.js
@@ -3,7 +3,7 @@ import { OFF, LIGHT_TRANSITION_MAPPING } from "./constants";
 /* REMARKS:
 **  Epic fail in that this is not a state machine and does not follow the pattern at all
 **  The dispatch, transition and action to reducer seemed important but before I did this, I only 
-*.  looked at at gettign the state 
+*.  looked at at getting the state 
 */
 export class Flashlight {
 	constructor() {
