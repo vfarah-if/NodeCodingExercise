b019c02dafdb69157898b13075d686cc1877893c
vfarah-if <vincent.farah@codurance.com>
2020-12-04 17:15:55 +0000

    Created structured http responses

--- src/responses/index.js
+++ src/responses/index.js
@@ -1,47 +1,35 @@
-export const success = (message) => ({
+export const success = (message, data) => ({
 	status: 200,
 	name: "Success",
 	message: message || "Succeeded",
+	data,
 });
 
-export const created = (message) => ({
+export const created = (message, data) => ({
 	status: 201,
 	name: "Created",
-	message: message || "Created",
+    message: message || "Created",
+    data,
 });
 
-export const badRequest = (message) => ({
+export const badRequest = (message, errors) => ({
 	status: 400,
 	name: "BadRequestError",
 	message: message || "Bad Request",
+	errors,
 });
 
-export const unauthorized = (message) => ({
-    status: 401,
-    name: "UnauthorizedRequestError",
-	message: message || "Unauthorized",
-});
+
 
 export const notFound = (message) => ({
-    status: 404,
-    name: "NotFoundError",
+	status: 404,
+	name: "NotFoundError",
 	message: message || "Resource Not Found",
 });
 
-export const noContent = (message) => ({
-    status: 204,
-    name: "NotContentError",
-	message: message || "No Data Imported",
-});
-
-export const unprocessable = (message) => ({
-    status: 422,
-    name: "UnprocessableError",
-	message: message || "Unprocessable",
-});
 
 export const internalServerError = (message) => ({
-    status: 500,
-    name: "InternalServerError",
+	status: 500,
+	name: "InternalServerError",
 	message: message || "Internal Server Error",
 });
