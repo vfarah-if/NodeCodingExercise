30500fe954b19f00f1c598c4948b3fe0ddcbb867
vfarah-if <vincent.farah@codurance.com>
2020-12-04 08:47:53 +0000

    Created all response types needed

---
+++ src/responses/index.js
@@ -0,0 +1,47 @@
+export const success = (message) => ({
+	status: 200,
+	name: "Success",
+	message: message || "Succeeded",
+});
+
+export const created = (message) => ({
+	status: 200,
+	name: "Created",
+	message: message || "Created",
+});
+
+export const badRequest = (message) => ({
+	status: 400,
+	name: "BadRequestError",
+	message: message || "Bad Request",
+});
+
+export const unauthorized = (message) => ({
+    status: 401,
+    name: "UnauthorizedRequestError",
+	message: message || "Unauthorized",
+});
+
+export const notFound = (message) => ({
+    status: 404,
+    name: "NotFoundError",
+	message: message || "Resource Not Found",
+});
+
+export const noContent = (message) => ({
+    status: 204,
+    name: "NotContentError",
+	message: message || "No Data Imported",
+});
+
+export const unprocessable = (message) => ({
+    status: 422,
+    name: "UnprocessableError",
+	message: message || "Unprocessable",
+});
+
+export const internalServerError = (message) => ({
+    status: 500,
+    name: "InternalServerError",
+	message: message || "Internal Server Error",
+});
