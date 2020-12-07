4c46d78d06565d3cffd658bf315b5c7a676664bd
vfarah-if <vincent.farah@codurance.com>
2020-12-07 10:15:24 +0000

    Added romanNumeral kata back

--- src/romanNumeralKata.js
+++ src/romanNumeralKata.js
@@ -1,9 +1,93 @@
-export function convertLatinNumberToRomanNumerals(latinNumber) {    
-	if (latinNumber === 2) {
-		return "II";
-	} else if (latinNumber === 3){
-        return "III";
-    }
+export const convertArabicNumberToRomanNumeral = (number) => {
+	const results = {
+		1: "I",
+		// 2: "II",
+		// 3: "III",
+		4: "IV",
+		5: "V",
+		// 6: "VI",
+		// 7: "VII",
+		// 8: "VIII",
+		9: "IX",
+		10: "X",
+		// 11: "XI"
+	};
+	const result = results[number];
+	console.debug(result);
+	if (result) {
+		return result;
+	}
 
-	return "I";
-}
+	if (number > 10) {
+		return "X" + convertArabicNumberToRomanNumeral(number - 10);
+	}
+
+	if (number > 5) {
+		return "V" + convertArabicNumberToRomanNumeral(number - 5);
+	}
+
+	return results[1] + convertArabicNumberToRomanNumeral(number - 1);
+
+	// Stage 1
+	// const result = results[number];
+	// console.debug(result);
+	// return result;
+};
+
+export const convertRomanNumeralToArabicNumber = (romanNumeral) => {
+	// Stage 1
+	// if (romanNumeral === "I") {
+	// 	return 1;
+	// } else {
+	// 	return 2;
+	// }
+
+	// Stage 2
+	// switch (romanNumeral) {
+	// 	case "I":
+	// 		return 1;
+	// 	case "II":
+	// 		return 2;
+	// 	case "III":
+	// 		return 3;
+	// 	default:
+	// 		break;
+	// }
+
+	// Stage 3
+	// const mappings = {
+	// 	I: 1,
+	// 	II: 2,
+	//  III: 3,
+	//  IV: 4,
+	//  V: 5,
+	//  VI: 6,
+	// };
+	// const result = mappings[romanNumeral];
+	// return result;
+
+	// Stage 4 and 5
+	const mappings = {
+		I: 1,
+		// II: 2,
+		// III: 3,
+		IV: 4,
+		V: 5,
+		// VI: 6,
+		// VII: 7,
+		// VIII: 8,
+	};
+
+	const result = mappings[romanNumeral];
+	if (result) {
+		return result;
+	}
+
+	if (romanNumeral.startsWith("V")) {
+		return 5 + convertRomanNumeralToArabicNumber(romanNumeral.slice(1));
+	}
+
+	const removeFirstChar = romanNumeral.slice(1);
+	console.debug(removeFirstChar);
+	return 1 + convertRomanNumeralToArabicNumber(removeFirstChar);
+};
--- src/romanNumeralKata.test.js
+++ src/romanNumeralKata.test.js
@@ -1,21 +1,59 @@
-import { convertLatinNumberToRomanNumerals } from "./romanNumeralKata";
+import { convertArabicNumberToRomanNumeral, convertRomanNumeralToArabicNumber } from "./romanNumeralKata";
 
-describe("convertLatinNumberToRomanNumerals", () => {
-	test("should convert 1 to I", () => {
-		const actual = convertLatinNumberToRomanNumerals(1);
+describe("convertArabicNumberToRomanNumeral", () => {
+	// test(`should convert '1' to 'I'`, () => {
+	// 	const actual = convertArabicNumberToRomanNumeral(1);
+	// 	expect(actual).toBe("I");
+	// });
 
-		expect(actual).toBe("I");
+	// test(`should convert '1' to 'I'`, () => {
+	// 	const actual = convertArabicNumberToRomanNumeral(2);
+	// 	expect(actual).toBe("II");
+	// });
+
+	test.each([
+		[1, "I"],
+		[2, "II"],
+		[3, "III"],
+		[4, "IV"],
+		[5, "V"],
+		[6, "VI"],
+		[7, "VII"],
+		[8, "VIII"],
+		[9, "IX"],
+		[10, "X"],
+		[11, "XI"],
+	])("should convert %d to %s", (number, expected) => {
+		console.debug("Converting", number, expected);
+		const actual = convertArabicNumberToRomanNumeral(number);
+		expect(actual).toBe(expected);
+	});
+});
+
+describe("convertRomanNumeralToArabicNumber", () => {
+	test("should convert I to 1", () => {
+		const actual = convertRomanNumeralToArabicNumber("I");
+		expect(actual).toBe(1);
 	});
 
-	test("should convert '2' to 'II'", () => {
-		const actual = convertLatinNumberToRomanNumerals(2);
-
-		expect(actual).toBe("II");
+	test("should convert II to 2", () => {
+		const actual = convertRomanNumeralToArabicNumber("II");
+		expect(actual).toBe(2);
 	});
 
-	test("should convert '3' to 'III'", () => {
-		const actual = convertLatinNumberToRomanNumerals(3);
+	test.each([
+		["I", 1],
+		["II", 2],
+		["III", 3],
+		["IV", 4],
+		["V", 5],
+		["VI", 6],
+		["VII", 7],
+		["VIII", 8],
 
-		expect(actual).toBe("III");
+	])(`should convert '%s' to '%d'`, (romanNumeral, expected) => {
+		console.debug(romanNumeral, expected);
+		const actual = convertRomanNumeralToArabicNumber(romanNumeral);
+		expect(actual).toBe(expected);
 	});
 });
