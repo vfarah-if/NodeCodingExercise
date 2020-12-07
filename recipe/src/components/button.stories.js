217b063f8656dbceb1221ac5605d357a538296ea
vfarah-if <vincent.farah@codurance.com>
2020-12-07 17:58:06 +0000

    Removed rubbish test

--- recipe/src/components/button.stories.js
+++ recipe/src/components/button.stories.js
@@ -3,42 +3,34 @@ import { Button } from './Button';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    title: 'Example/Button',
-    component: Button,
-    argTypes: {
-      backgroundColor: { control: 'color' },
-    },
-  };
-  
-  const Template = (args) => <Button {...args} />;
-  
-  export const StoryOne = Template.bind({});
-  StoryOne.args = {
-    primary: true,
-    displayName: 'Button',
-  };
-  
-  export const StoryTwo = Template.bind({});
-  StoryTwo.args = {
-    displayName: 'Button',
-  };
-  
-  export const StoryThree = Template.bind({});
-  StoryThree.args = {
-    size: 'large',
-    displayName: 'Button',
-  };
-  
-  export const StoryFour = Template.bind({});
-  StoryFour.args = {
-    size: 'small',
-    displayName: 'Button',    
-  };
+	title: 'Example/Button',
+	component: Button,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
+};
 
-  export const SmallWithClick = Template.bind({});
-  SmallWithClick.args = {
-    size: 'small',
-    displayName: 'Button',    
-    onClick: 'alert("test")'
-  };
-  
+const Template = (args) => <Button {...args} />;
+
+export const StoryOne = Template.bind({});
+StoryOne.args = {
+	primary: true,
+	displayName: 'Button',
+};
+
+export const StoryTwo = Template.bind({});
+StoryTwo.args = {
+	displayName: 'Button',
+};
+
+export const StoryThree = Template.bind({});
+StoryThree.args = {
+	size: 'large',
+	displayName: 'Button',
+};
+
+export const StoryFour = Template.bind({});
+StoryFour.args = {
+	size: 'small',
+	displayName: 'Button',
+};
