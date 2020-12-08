3cc9918e64ecb4915c8cc6f0785a3e9bf5bdc22c
vfarah-if <vincent.farah@codurance.com>
2020-12-08 14:41:23 +0000

    Remove the real map so app no slowed down

---
+++ recipe/src/components/Map/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Map({center}) {
+    return (
+        <div>
+            <p>Represents a map with '{center}' configured</p>
+        </div>    
+    )
+}
