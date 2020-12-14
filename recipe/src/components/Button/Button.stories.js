9c90c1c1f259c01a8dce975d742bbacc633fe1ef
Vincent Farah <sefrica@gmail.com>
2020-12-14 21:44:21 +0000

    Update button tests

--- recipe/src/components/Button/Button.stories.js
+++ recipe/src/components/Button/Button.stories.js
@@ -4,34 +4,29 @@ import { action } from '@storybook/addon-actions';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-	title: 'Example/Button',
+	title: 'Component/Button',
 	component: Button,
 	argTypes: {
 		backgroundColor: { control: 'color' },
 	},
 };
 
-const ButtonTemplate = (args) => <Button {...args} onClick={action('onClicked...')} />;
+const ButtonTemplate = (args) => <Button displayName="Button" {...args} onClick={action('onClicked...')} />;
 
-export const StoryOne = ButtonTemplate.bind({});
-StoryOne.args = {
-	primary: true,
-	displayName: 'Button',
+export const PrimaryButtonStory = ButtonTemplate.bind({});
+PrimaryButtonStory.args = {
+	primary: true
 };
 
-export const StoryTwo = ButtonTemplate.bind({});
-StoryTwo.args = {
-	displayName: 'Button',
+export const DefaultButtonStory = ButtonTemplate.bind({});
+DefaultButtonStory.args = {};
+
+export const LargeButtonStory = ButtonTemplate.bind({});
+LargeButtonStory.args = {
+	size: 'large'
 };
 
-export const StoryThree = ButtonTemplate.bind({});
-StoryThree.args = {
-	size: 'large',
-	displayName: 'Button',
-};
-
-export const StoryFour = ButtonTemplate.bind({});
-StoryFour.args = {
-	size: 'small',
-	displayName: 'Button',
+export const SmallButtonStory = ButtonTemplate.bind({});
+SmallButtonStory.args = {
+	size: 'small'
 };
--- recipe/src/components/Button/Button.test.js
+++ recipe/src/components/Button/Button.test.js
@@ -2,12 +2,50 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import { StoryOne } from './Button.stories';
+import {
+	PrimaryButtonStory,
+	DefaultButtonStory,
+	SmallButtonStory,
+	LargeButtonStory,
+} from './Button.stories';
 
 describe('button', () => {
-	test('should create a button with Button display name', () => {
-		render(<StoryOne {...StoryOne.args} />);
+	test('should create a button with a primary Button display name', () => {
+		const { container } = render(
+			<PrimaryButtonStory {...PrimaryButtonStory.args} />
+		);
 
 		expect(screen.getByRole('button')).toHaveTextContent('Button');
+		expect(container.querySelector('button')).toMatchSnapshot();
+	});
+
+	test('should create a button simple Button display name', () => {
+		const renderResponse = render(
+			<DefaultButtonStory {...DefaultButtonStory.args} />
+		);
+
+		expect(
+			renderResponse.container.querySelector('button')
+		).toMatchSnapshot();
+	});
+
+	test('should create a button large Button display name', () => {
+		const renderResponse = render(
+			<LargeButtonStory {...LargeButtonStory.args} />
+		);
+
+		expect(
+			renderResponse.container.querySelector('button')
+		).toMatchSnapshot();
+	});
+
+	test('should create a button small Button display name', () => {
+		const renderResponse = render(
+			<SmallButtonStory {...SmallButtonStory.args} />
+		);
+
+		expect(
+			renderResponse.container.querySelector('button')
+		).toMatchSnapshot();
 	});
 });
