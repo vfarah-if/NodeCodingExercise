3447676bba70147e27132aaf6bfc7b327177e706
Vincent Farah <sefrica@gmail.com>
2020-12-14 15:46:19 +0000

    Refactored Header Test to use @testing-library library and upgraded documentation

--- recipe/src/components/Header/Header.test.js
+++ recipe/src/components/Header/Header.test.js
@@ -1,106 +1,83 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import '@testing-library/jest-dom';
+import { render, fireEvent } from '@testing-library/react';
 
 import Header from '../Header';
 
 describe('Header', () => {
-	let container, loginHandler, logoutHandler, createAccountHandler;
+	let loginHandler, logoutHandler, createAccountHandler;
 
 	beforeEach(() => {
 		loginHandler = jest.fn();
 		logoutHandler = jest.fn();
 		createAccountHandler = jest.fn();
-		container = document.createElement('div');
-		document.body.appendChild(container);
-	});
-
-	afterEach(() => {
-		unmountComponentAtNode(container);
-		container.remove();
-		container = null;
 	});
 
 	it('should render header with login and sign up when no user assigned', () => {
-		act(() => {
-			render(
-				<Header
-					onLogin={loginHandler}
-					onLogout={logoutHandler}
-					onCreateAccount={createAccountHandler}
-				/>,
-				container
-			);
-		});
+		const container = render(
+			<Header
+				onLogin={loginHandler}
+				onLogout={logoutHandler}
+				onCreateAccount={createAccountHandler}
+			/>
+		);
 
-		expect(container.textContent).toContain('Log in');
-		expect(container.textContent).toContain('Sign up');
-		expect(container.textContent).not.toContain('Log out');
-    });
-    
-    it('should login a user or sign up appropriatly', () => {
-		act(() => {
-			render(
-				<Header
-					onLogin={loginHandler}
-					onLogout={logoutHandler}
-					onCreateAccount={createAccountHandler}
-				/>,
-				container
-			);
-		});
+		expect(container.getByText('Log in')).toBeInTheDocument();
+		expect(container.getByText('Sign up')).toBeInTheDocument();
+		expect(container.queryByText('Log out')).toBeNull();
+	});
 
-        const buttons = container.querySelectorAll('button');
-        expect(buttons.length).toBe(2);
-        buttons[0].click();
-        buttons[1].click();
+	it('should login a user or sign up appropriatly', () => {
+		const container = render(
+			<Header
+				onLogin={loginHandler}
+				onLogout={logoutHandler}
+				onCreateAccount={createAccountHandler}
+			/>
+		);
 
-        expect(loginHandler).toBeCalled();
-        expect(createAccountHandler).toBeCalled();
-        expect(logoutHandler).not.toBeCalled();
+		fireEvent.click(container.getByText('Log in'));
+		fireEvent.click(container.getByText('Sign up'));
+
+		expect(loginHandler).toBeCalled();
+		expect(createAccountHandler).toBeCalled();
+		expect(logoutHandler).not.toBeCalled();
 	});
 
 	it('should render header with logout when user is assigned', () => {
 		const user = {};
 
-		act(() => {
-			render(
-				<Header
-					user={user}
-					onLogin={loginHandler}
-					onLogout={logoutHandler}
-					onCreateAccount={createAccountHandler}
-				/>,
-				container
-			);
-		});
+		const container = render(
+			<Header
+				user={user}
+				onLogin={loginHandler}
+				onLogout={logoutHandler}
+				onCreateAccount={createAccountHandler}
+			/>
+		);
 
-		expect(container.textContent).not.toContain('Log in');
-		expect(container.textContent).not.toContain('Sign up');
-		expect(container.textContent).toContain('Log out');
-    });
-    
-    it('should logout user when button clicked', () => {
-        const user = {};
+		expect(container.queryByText('Log in')).not.toBeInTheDocument();
+		expect(container.queryByText('Sign up')).not.toBeInTheDocument();
+		expect(container.getByText('Log out')).toBeInTheDocument();
+	});
 
-		act(() => {            
-			render(
-				<Header
-                    user={user}
-					onLogin={loginHandler}
-					onLogout={logoutHandler}
-					onCreateAccount={createAccountHandler}
-				/>,
-				container
-			);
-		});
+	it('should logout user when button clicked', () => {
+		const user = {};
 
-        const buttons = container.querySelectorAll('button');
-        expect(buttons.length).toBe(1);
-        buttons[0].click();
+		const container = render(
+			<Header
+				user={user}
+				onLogin={loginHandler}
+				onLogout={logoutHandler}
+				onCreateAccount={createAccountHandler}
+			/>
+		);
 
-        expect(loginHandler).not.toBeCalled();
-        expect(createAccountHandler).not.toBeCalled();
-        expect(logoutHandler).toBeCalled();
+		const logoutButton = container.getByText('Log out');
+		fireEvent.click(logoutButton);
+
+		expect(loginHandler).not.toBeCalled();
+		expect(createAccountHandler).not.toBeCalled();
+		expect(logoutHandler).toBeCalled();
 	});
 });
