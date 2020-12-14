145179dd06d81cd0d9b8bd792572b5a27f4e4758
Vincent Farah <sefrica@gmail.com>
2020-12-14 17:10:50 +0000

    Updated Hello Test

--- recipe/src/components/Hello/hello.test.js
+++ recipe/src/components/Hello/hello.test.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 import Hello from './Hello';
 
 describe('Hello', () => {
-    let container;
-
-    beforeEach(() => {
-        container = document.createElement('div');
-        document.body.appendChild(container);
-    })
-
-    afterEach(() => {
-        unmountComponentAtNode(container);
-        container.remove();
-        container = null;
-    })
-
     test('should say hello stranger when no name assigned', () => {
-        render(<Hello/>, container);
+        const container = render(<Hello/>);
 
-        expect(container.textContent).toContain('Hello stranger')
+        expect(container.getByText('Hello stranger')).toBeInTheDocument();
     });
 
     test('should say hello to the person when name is assigned', () => {
-        render(<Hello name="Jane Doe"/>, container);
+        const container = render(<Hello name="Jane Doe"/>);
 
-        expect(container.textContent).toContain('Hello Jane Doe')
+        expect(container.getByText('Hello Jane Doe')).toBeInTheDocument()
     });
 
 });
