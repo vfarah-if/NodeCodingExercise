97c4ef605c1cf02342ee79158b76329248bdb088
Vincent Farah <sefrica@gmail.com>
2020-12-14 17:19:44 +0000

    Refactored simple hello component

---
+++ recipe/src/components/Hello/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style/index.css';
+import PropTypes from 'prop-types';
+
+export default function Hello({ name }) {
+	return <h1 className="name">Hello {name || 'stranger'}</h1>;
+}
+
+Hello.propTypes = {
+	name: PropTypes.string,
+};
