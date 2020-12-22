1891c1ddcc237f1564ef38a2ffce20a81a7a985b
Vincent Farah <sefrica@gmail.com>
2020-12-22 11:46:40 +0000

    Updated redux as an experiment

--- recipe-ts/src/App.tsx
+++ recipe-ts/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import Header from './components/Header';
 import MarsRoverImages from './components/MarsRoverImages';
+import ListReducer, { reducer } from './components/ListReducer'
 
 import './App.css';
+
+const rootReducer = combineReducers(reducer);
+const store = createStore(rootReducer);
 
 function App() {
 	let user = undefined;
@@ -12,14 +18,16 @@ function App() {
 
 	return (
 		<div className="App">
-
-				<Header		
-					user={user}
-					onLogin={loginHandler}
-					onCreateAccount={createAccountHandler}
-				/>
-				<br/>
-				<MarsRoverImages/>
+			<Header
+				user={user}
+				onLogin={loginHandler}
+				onCreateAccount={createAccountHandler}
+			/>
+			<br />
+			<MarsRoverImages />
+			<Provider store={store}>
+				<ListReducer/>
+			</Provider>
 		</div>
 	);
 }
