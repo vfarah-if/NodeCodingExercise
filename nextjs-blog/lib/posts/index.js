49557844b386d8cb407c19976569343330f39fd7
Vincent Farah <sefrica@gmail.com>
2020-12-24 00:42:12 +0000

    Created blog posts from file directory

---
+++ nextjs-blog/lib/posts/index.js
@@ -0,0 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter'
+
+const postsDirectory = path.join('./lib', 'posts');
+
+export function getSortedPostsData() {
+    console.debug(postsDirectory);
+    const fileNames = fs.readdirSync(postsDirectory);
+    console.debug('File Names', fileNames);
+    const allPostsData = fileNames
+        .filter(fileName => path.extname(fileName).endsWith('md'))
+        .map(fileName => {
+        const id = fileName.replace(/\md$/, '');
+        const fullPath = path.join(postsDirectory, fileName);
+
+        const fileContents = fs.readFileSync(fullPath, 'utf-8');
+        const matterResult = matter(fileContents);
+        return {
+            id,
+            ...matterResult.data
+        }
+    })
+
+    return allPostsData.sort((a, b) => (a.date < b.date) ? 1 : -1);
+}
+
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,31 +2,55 @@ import Head from 'next/head'
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/posts/first-post">
-              <a>First Post</a>
-            </Link>
-          </li>          
-        </ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/posts/first-post">
+            <a>First Post</a>
+          </Link>
+        </li>
+      </ul>
       <section className={utilStyles.headingMd}>
         <p>Having worked with Next JS without doing any tutorials, I thought I would go back and take nothign for granted.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blogs found '{allPostsData.length}'</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
