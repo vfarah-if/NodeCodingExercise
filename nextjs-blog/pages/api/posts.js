d0276a44cf93b9e699b13b46b9967ef64f78db0e
Vincent Farah <sefrica@gmail.com>
2020-12-26 18:41:15 +0000

    Create api's

---
+++ nextjs-blog/pages/api/posts.js
@@ -0,0 +1,7 @@
+import { getSortedPostsData } from '../../lib/posts';
+
+export default function handler(req, res) {
+	const id = req.body.id;
+	console.debug(req);
+	res.status(200).json(getSortedPostsData(id));
+}
