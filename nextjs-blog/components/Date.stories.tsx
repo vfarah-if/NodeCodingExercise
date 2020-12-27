076523e573260f3c8b158c30eb78d47e03a5d1ec
Vincent Farah <sefrica@gmail.com>
2020-12-27 18:39:01 +0000

    Created Next JS project like react typescript

---
+++ nextjs-blog/components/Date.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Date, { DateProps } from './date';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+export default { title: 'Component/Date', component: Date } as Meta;
+
+const DateTemplate: Story<DateProps> = (args: DateProps) => (
+	<>
+		<Date {...args} />
+	</>
+);
+
+export const EmptyDate = DateTemplate.bind({});
+EmptyDate.args = {
+	dateString: undefined,
+};
+
+export const DefaultDate = DateTemplate.bind({});
+DefaultDate.args = {
+	dateString: '2011-10-05T14:48:00.000Z',
+};
