1e72a68dc51000d8f5664927043acadff864ff5a
Vincent Farah <sefrica@gmail.com>
2020-12-27 13:08:05 +0000

    Refactored more to typescript

---
+++ nextjs-blog/lib/posts/index.tsx
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join('./lib', 'posts');
+
+export interface Post {
+    contentHtml: string;
+    id: string;    
+    date: Date;
+    title: string;
+}
+
+export function getSortedPostsData(id: string | undefined): Array<Post> | Post {
+	const fileNames = fs.readdirSync(postsDirectory);
+	console.debug('File Names', fileNames);
+	const allPostsData = fileNames
+		.filter((fileName) => path.extname(fileName).endsWith('md'))
+		.map((fileName) => {
+			const id = fileName.replace(/\.md$/, '');
+			const fullPath = path.join(postsDirectory, fileName);
+
+			const fileContents = fs.readFileSync(fullPath, 'utf-8');
+			const matterResult = matter(fileContents);
+			return {
+				id,
+				contentHtml: id && matterResult.content,
+				...matterResult.data,
+			} as Post;
+		});
+
+	return id
+		? allPostsData.find((post) => post.id === id)
+		: allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
--- nextjs-blog/pages/api/posts.tsx
+++ nextjs-blog/pages/api/posts.tsx
@@ -1,6 +1,7 @@
 import { getSortedPostsData } from '../../lib/posts';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req, res) {
+export default function (req: NextApiRequest, res: NextApiResponse) {
 	const id = req.body.id;
 	// console.debug(req);
 	res.status(200).json(getSortedPostsData(id));
