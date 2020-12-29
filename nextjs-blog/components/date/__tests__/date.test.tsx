a87ae157ff7287c4fc300e8a2f8fe819d726701f
Vincent Farah <sefrica@gmail.com>
2020-12-29 11:59:16 +0000

    Added tests to next js as simply as possible

--- nextjs-blog/components/date/__tests__/date.test.tsx
+++ nextjs-blog/components/date/__tests__/date.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { EmptyDate } from '../date.stories';
+import { EmptyDate, DefaultDate } from '../date.stories';
 
-describe('date', () => {
+describe('Date tests as defined by stories', () => {
 	test('should generate a no time assigned message when nothing assigned', () => {
 		const component = render(<EmptyDate />);
+
+		expect(component.container).toMatchSnapshot();
+	});
+
+	test('should generate a default date when assigned all expected values', () => {
+		const component = render(<DefaultDate dateString={DefaultDate.args.dateString} />);
 
 		expect(component.container).toMatchSnapshot();
 	});
