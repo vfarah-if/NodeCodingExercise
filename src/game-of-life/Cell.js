bf9829099c58c557a27c6b282b1bcc51a8f27d45
Vincent Farah <vincent.farah@madetech.com>
2021-07-09 18:26:32 +0100

    Create 3 live neighbours based on reporduction rules

--- src/game-of-life/Cell.js
+++ src/game-of-life/Cell.js
@@ -3,6 +3,27 @@ const { CellState } = require("./CellState");
 class Cell {
   constructor(state = CellState.Dead) {
     this.currentState = state;
+    this.neighbours = new Array();
+  }
+
+  addNeighbours(items) {
+    items?.forEach((item) => {
+      this.neighbours.push(item);
+    });
+  }
+
+  nextState() {
+    console.log(this.neighbours);
+    const liveNeighbours = this.neighbours.filter(
+      (item) => item.currentState === CellState.Alive
+    );
+
+    let result = this.currentState;
+    console.log(liveNeighbours);
+    if (liveNeighbours.length === 3 && this.currentState === CellState.Dead) {
+      result = CellState.Alive;
+    }
+    return result;
   }
 }
 
--- src/game-of-life/cell.test.js
+++ src/game-of-life/cell.test.js
@@ -8,4 +8,19 @@ describe("cell", () => {
     expect(cell.currentState).toBe(CellState.Dead);
     expect(cell.currentState === CellState.Dead).toBeTruthy();
   });
+
+  test("should come to life with three live neighbours by virtue of reproduction", () => {
+    const cell = new Cell();
+    cell.addNeighbours(
+      new Array(
+        new Cell(CellState.Alive),
+        new Cell(CellState.Alive),
+        new Cell(CellState.Alive)
+      )
+    );
+
+    const actual = cell.nextState();
+
+    expect(actual).toBe(CellState.Alive);
+  });
 });
