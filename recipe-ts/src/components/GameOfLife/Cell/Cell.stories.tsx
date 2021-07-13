fc90ee0e4dc03588239f6d32084868841ac82c34
Vincent Farah <vincent.farah@madetech.com>
2021-07-13 10:36:44 +0100

    Configured Scenario

--- recipe-ts/src/components/GameOfLife/Cell/Cell.stories.tsx
+++ recipe-ts/src/components/GameOfLife/Cell/Cell.stories.tsx
@@ -1,17 +1,28 @@
+import { action } from '@storybook/addon-actions';
 import { Meta, Story } from '@storybook/react/types-6-0';
-import React from 'react';
 import Cell, { CellProps } from './Cell';
 
 export default {
   title: 'Component/GameOfLife/Cell',
   component: Cell,
+  argTypes: {
+    cellBorderColor: { control: 'color' },
+    cellBackgroundColor: { control: 'color' },
+  },
 } as Meta;
 
 const CellTemplate: Story<CellProps> = (args) => (
   <>
-    <Cell {...args} />
+    <Cell {...args} onCellClick={action('onCellClicked...')} />
   </>
 );
 
 export const DefaultCell = CellTemplate.bind({});
 DefaultCell.args = {};
+
+export const ConfiguredFlashyCell = CellTemplate.bind({});
+ConfiguredFlashyCell.args = {
+  cellSize: 50,
+  cellBorderColor: 'orange',
+  cellBackgroundColor: 'red',
+};
--- recipe-ts/src/components/GameOfLife/Cell/Cell.tsx
+++ recipe-ts/src/components/GameOfLife/Cell/Cell.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import './style/index.css';
 
-export interface CellProps {}
+export interface CellProps {
+  cellSize: number;
+  cellBorderColor: string;
+  cellBackgroundColor: string;
+  onCellClick: MouseEventHandler;
+}
 
-const Cell: React.FC<CellProps> = (props) => {
-  return <>TODO</>;
+const Cell: React.FC<CellProps> = ({
+  onCellClick,
+  cellSize = 40,
+  cellBorderColor = 'blue',
+  cellBackgroundColor = undefined,
+}) => {
+  return (
+    <div
+      className='cell'
+      onClick={onCellClick}
+      style={{
+        width: cellSize,
+        height: cellSize,
+        border: `solid 2px ${cellBorderColor}`,
+        backgroundColor: cellBackgroundColor,
+      }}
+    ></div>
+  );
 };
 
 export default Cell;
