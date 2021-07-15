6729312b017e58d41b4410d3913fe8d2e70dc10d
Vincent Farah <vincent.farah@madetech.com>
2021-07-15 09:22:01 +0100

    Create test for CreateFiveByFiveBoardOfEmptyCells

--- recipe-ts/src/components/GameOfLife/__tests__/GameOfLife.test.tsx
+++ recipe-ts/src/components/GameOfLife/__tests__/GameOfLife.test.tsx
@@ -1,12 +1,28 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import { DefaultBoardOfEmptyCells } from '../GameOfLife.stories';
+import {
+  DefaultBoardOfEmptyCells,
+  CreateFiveByFiveBoardOfEmptyCells,
+} from '../GameOfLife.stories';
 
 describe('GameOfLife', () => {
   describe('Given a default board', () => {
     test('should create a default sized empty board with all expected controls', () => {
       const { container } = render(<DefaultBoardOfEmptyCells />);
+
+      expect(container).toMatchSnapshot();
+    });
+
+    test('should configure a five by five board of empty cells with specific settings', () => {
+      const { container } = render(
+        <CreateFiveByFiveBoardOfEmptyCells
+          boardSize={CreateFiveByFiveBoardOfEmptyCells.args?.boardSize}
+          cellSize={CreateFiveByFiveBoardOfEmptyCells.args?.cellSize}
+          gridColor={CreateFiveByFiveBoardOfEmptyCells.args?.gridColor}
+          activeColor={CreateFiveByFiveBoardOfEmptyCells.args?.activeColor}
+        />
+      );
 
       expect(container).toMatchSnapshot();
     });
