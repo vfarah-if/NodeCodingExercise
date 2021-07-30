ca98422bc510a7c28388e2a04655f243afcd937d
Vincent Farah <vincent.farah@madetech.com>
2021-07-30 08:57:06 +0100

    Started am empty GameOfLife to do with Chloe

---
+++ recipe/src/components/GameOfLife/GameOfLife.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style/index.css';
+
+const GameOfLife = (props) => {
+  return <>TODO: With Chloe</>;
+};
+
+export default GameOfLife;
---
+++ recipe/src/components/GameOfLife/GameOfLife.stories.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import GameOfLife from './GameOfLife';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: 'Component/GameOfLife',
+    component: GameOfLife,
+};
+
+export const GameOfLifeExample = () => (
+    <>
+        <GameOfLife />
+    </>
+);
