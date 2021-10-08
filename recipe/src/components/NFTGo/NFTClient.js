6f9325cf918ae103495c83470d4503f1b0102d68
Vincent Farah <vincent.farah@madetech.com>
2021-10-08 10:36:04 +0100

    Updated arrow syntax to be consistent (demo function)

--- recipe/src/components/NFTGo/NFTClient.js
+++ recipe/src/components/NFTGo/NFTClient.js
@@ -2,9 +2,9 @@ const NFTGO_API_V1_URL = 'https://api.nftgo.io/api/v1';
 const COLLECTIONS_URL = `${NFTGO_API_V1_URL}/collections`;
 const SYSTEM_URL = `${NFTGO_API_V1_URL}/system`;
 
-export function getHotCollection(limit = 10, withStat = 1) {
+export const getHotCollection = (limit = 10, withStat = 1) => {
   return fetch(`${COLLECTIONS_URL}/hot?limit=${limit}&withStat=${withStat}`);
-}
+};
 
 export const getBlockChains = () => {
   return fetch(`${SYSTEM_URL}/parameters`);
