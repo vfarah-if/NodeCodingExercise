d76d0ef9de0ddfa43e79d363e1a6fe66cc5ed9a7
Vincent Farah <vincent.farah@madetech.com>
2021-10-09 13:54:50 +0100

    Added Limit and TODO

--- recipe/src/components/NFTGo/NFTClient.js
+++ recipe/src/components/NFTGo/NFTClient.js
@@ -5,6 +5,9 @@ const TOP_SALES_URL = `${NFTGO_API_V1_URL}/asset/top-sales`;
 const LATEST_DEAL_URL = `${NFTGO_API_V1_URL}/asset/latestdeal`;
 const RECENTLY_CREATED_URL = `${NFTGO_API_V1_URL}/assets/recently-created`;
 
+// TODO: Refactor pagination into default options
+// TODO: Remove duplication where possible
+
 export const getHotCollection = (limit = 10, withStat = 1) => {
   return fetch(`${COLLECTIONS_URL}/hot?limit=${limit}&withStat=${withStat}`);
 };
@@ -17,23 +20,29 @@ export const getBlockChains = () => {
 export const getTopSales = (
   limit = 10,
   timesSpan = '24h',
-  cid = '6125b9851722e60a4a7f864c'
+  cid = '6125b9851722e60a4a7f864c',
+  offset = 0
 ) => {
   return fetch(
-    `${TOP_SALES_URL}?limit=${limit}&timespan=${timesSpan}&cid=${cid}`
+    `${TOP_SALES_URL}?limit=${limit}&timespan=${timesSpan}&cid=${cid}&offset=${offset}`
   );
 };
 
-export const getLatestDeal = (limit = 10, cid = '6125b9851722e60a4a7f864c') => {
-  return fetch(`${LATEST_DEAL_URL}?limit=${limit}&cid=${cid}`);
+export const getLatestDeal = (
+  limit = 10,
+  cid = '6125b9851722e60a4a7f864c',
+  offset = 0
+) => {
+  return fetch(`${LATEST_DEAL_URL}?limit=${limit}&cid=${cid}&offset=${offset}`);
 };
 
 export const getRecentlyCreated = (
   limit = 10,
   withCollection = 1,
-  cid = '6125b9851722e60a4a7f864c'
+  cid = '6125b9851722e60a4a7f864c',
+  offset = 0
 ) => {
   return fetch(
-    `${RECENTLY_CREATED_URL}?limit=${limit}&withCollection=${withCollection}&cid=${cid}`
+    `${RECENTLY_CREATED_URL}?limit=${limit}&withCollection=${withCollection}&cid=${cid}&offset=${offset}`
   );
 };
