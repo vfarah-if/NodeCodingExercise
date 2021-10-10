6721022e996f86c152c5f2eb671892cee9d626a7
Vincent Farah <vincent.farah@madetech.com>
2021-10-10 16:12:53 +0100

    Serialise files out better for analysis

---
+++ recipe/src/components/NFTGo/analysis/My Curio Cards Analytics _ NFTGO_files/_app-b48b79a8f9cc85e6e437.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[23],{10:function(n,_,o){o("74v/"),n.exports=o("nOHt")},"74v/":function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return o("cha2")}])}},[[10,0,1,6,3,2,4,5,7,8]]]);
---
+++ recipe/src/components/NFTGo/analysis/My Curio Cards Analytics _ NFTGO_files/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F404","\u002F500","\u002F502"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
