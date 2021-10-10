0d40b756ec33d885c65c70156773d44e8155facf
Vincent Farah <vincent.farah@madetech.com>
2021-10-10 15:28:32 +0100

    Test output

--- recipe/src/components/NFTGo/TopHolders/TopHolders.jsx
+++ recipe/src/components/NFTGo/TopHolders/TopHolders.jsx
@@ -1,8 +1,365 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './style/index.css';
 
 const TopHolders = (props) => {
-  return <Fragment></Fragment>;
+  return (
+    <div className='collection-whale_tableArea__3fC_D'>
+      <div className='table_rankTable__2HrNl'>
+        <div className='table_itemHead__13C7v'>
+          <svg
+            width='24'
+            height='19'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+            className='table_itemHeadIcon__lhevM'
+          >
+            <path
+              d='M23.822.065c-.761.982-2.466-.45-3.016 2.297 0 0-.24-.933-2.001-1.062-.59-.085-1.423-.047-2-.276-.265-.123-.464-.018-.41.286.441 1.582 1.784 1.172 2.766 1.856 1.131.727 1.624 2.205 1.1 3.516-.099.25-.232.483-.395.694a4.152 4.152 0 01-2.241.235 5.252 5.252 0 01-.593-.103c-2.07-.503-9.343-2.867-14.363-1.725a7.316 7.316 0 00-2.055.755l-.027.014a6.529 6.529 0 00-.581.367 10.471 10.471 0 00.032 1.217l.004.046.012.132c.023.244.05.462.078.652.32 2.34 1.536 5.646 4.522 7.846 3.395 2.442 6.818 2.272 8.457 2.015a10.373 10.373 0 004.473-1.725c1.528-1.035 2.325-2.103 2.325-2.103a12.455 12.455 0 002.09-3.546l.007-.014c.38-.971.56-1.802.575-2.306.25-1.702.093-3.253-.162-4.43-.074-.655-.413-1.12-.306-1.808.267-1.201 1.435-.31 1.848-2.378.056-.285-.03-.592-.14-.452z'
+              fill='#828282'
+            ></path>
+          </svg>
+          <div className='table_itemTitle__2SGro'>Top Holders</div>
+        </div>
+        <div className='head_rankHead__3S8tb'>
+          <div className='flex-box_flexBoxRow__2sC4D rank-head-content_right__3rAeh rank-head-content_container__3BOop'>
+            #
+          </div>
+          <div className='head_rankHeadAddress__3xsLG'>Address</div>
+          <div className='head_rankHeadNFTs__3DDFY'>NFTs</div>
+          <div className='head_rankHeadHoldingValue__2NXVy'>
+            <div className='head_holdValue__AVJcR'>
+              <div className='flex-box_flexBoxRow__2sC4D rank-head-content_right__3rAeh rank-head-content_container__3BOop rank-head-content_right__3rAeh rank-head-content_container__3BOop'>
+                <div className='flex-box_flexBoxColumn__3RsOR rank-head-content_headContent__2vVek'>
+                  <div className='flex-box_flexBoxRow__2sC4D rank-head-content_content__1J_yq'>
+                    <div>
+                      <svg
+                        width='13'
+                        height='5'
+                        viewBox='0 0 13 8'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='head_icon__X9DJJ'
+                      >
+                        <path
+                          d='M.579 1.684C-.021 1.045.433 0 1.309 0h10.383c.875 0 1.328 1.045.73 1.684L7.23 7.222a1 1 0 01-1.46 0L.58 1.684z'
+                          fill='#C4C4C4'
+                        ></path>
+                      </svg>
+                    </div>
+                    <span className='head_infoTitle__1wgh-'>Holding Value</span>
+                    <svg
+                      width='14'
+                      height='14'
+                      fill='none'
+                      xmlns='http://www.w3.org/2000/svg'
+                      className='head_infoIcon__1NiQI'
+                    >
+                      <path
+                        d='M7 0a7 7 0 100 14A7 7 0 007 0zm0 2.868a1.002 1.002 0 110 2.004 1.002 1.002 0 010-2.004zm1.503 8.266H5.496v-.5h.5v-4.51h-.5v-.5H8v5.01h.5v.5h.002z'
+                        fill='#BDBDBD'
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className='rank-head-content_underLine__7THed'></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='head_rankHeadBuyVolume__1r8mn'>Buy Volume</div>
+          <div className='head_rankHeadSellVolume__1nEKv'>Sell Volume</div>
+          <div className='head_rankHeadLastTrade__1tO4O'>Last Trade</div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>01</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0xbCf5ae...3BD2</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>624</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$11,351,898.36</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$19,125.96</div>
+          <div className='list-item_rankSellValue__2Q65B'>$3,527,695.48</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0x3dcfeaf6248c243b0fa385c0f7ef45d5fda97463fde12b280ddc13767097ad11'
+              target='_blank'
+            >
+              2021-08-30
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>02</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0xB618aa...A193</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>50</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$5,260,342</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$95,827.12</div>
+          <div className='list-item_rankSellValue__2Q65B'>$1,826,105.05</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0x8daf0d91bc20150d8b4502db4d78fd129840b73a563ca8349655d39a1e7cd3e1'
+              target='_blank'
+            >
+              1 day ago
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>03</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0x8A591E...f982</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>366</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$5,002,125.72</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$0</div>
+          <div className='list-item_rankSellValue__2Q65B'>$560,433.71</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0x533b91c7ab4db8c1767e3e2173debaf819958fcacd80238544c7cc4bf286d8cc'
+              target='_blank'
+              rel='noreferrer'
+            >
+              2021-09-23
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>04</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0x93dEF4...1323</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>188</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$4,932,406.06</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$0</div>
+          <div className='list-item_rankSellValue__2Q65B'>$77,828.44</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0xe20ba3555e5a62dbfeb0280050b0fd5b0bf14e96eca399d39ad62f0d7721d666'
+              target='_blank'
+              rel='noreferrer'
+            >
+              2021-09-16
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>05</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0x3A172e...457C</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>104</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$3,593,307.6</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$3,454,261.9</div>
+          <div className='list-item_rankSellValue__2Q65B'>$0</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0xa90ccfb32b3011a6c3a35f94edfa89bfb6342d80d4e9378c64ce406723c52f6c'
+              target='_blank'
+              rel='noreferrer'
+            >
+              2021-08-07
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>06</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0xA201d0...c709</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>143</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$3,520,030.26</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$618,370.84</div>
+          <div className='list-item_rankSellValue__2Q65B'>$724,110.12</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0xc4464184d134e2fc4ac5bb1f6efebaec0742ffc711ee3569449e08fca0b3321c'
+              target='_blank'
+              rel='noreferrer'
+            >
+              2021-08-30
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>07</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0x49468F...52aE</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>108</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$3,223,659.8</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$198,891.47</div>
+          <div className='list-item_rankSellValue__2Q65B'>$674,077.49</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0x75797cb922692d8f8da252e3bc62ff7200a5dfff4efb28a9b6309976ae9fb018'
+              target='_blank'
+              rel='noreferrer'
+            >
+              3 days ago
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>08</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0xEdae46...1CD1</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>39</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$2,888,215.83</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$65,589.84</div>
+          <div className='list-item_rankSellValue__2Q65B'>$318,713.32</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0x1115ef606cd61b07281d352bac3c24d5c0c0fd49f6a250cde639a8b46329f6f2'
+              target='_blank'
+              rel='noreferrer'
+            >
+              3 days ago
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>09</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0x2c9aEe...d358</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>175</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$2,833,151.68</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$0</div>
+          <div className='list-item_rankSellValue__2Q65B'>$8,464.91</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0x2be13459a453e0902f4da1f49e9a2a836dad14898e25c5849bc2d75ec751f16a'
+              target='_blank'
+              rel='noreferrer'
+            >
+              9 hours ago
+            </a>
+          </div>
+        </div>
+        <div className='flex-box_flexBoxRow__2sC4D list-item_rankItem__1ULb4'>
+          <div className='list-item_rankItemHash__IrAcT'>10</div>
+          <img
+            src='https://static.nftgo.io/asset/blockchain/eth.png'
+            className='list-item_image__5OONB'
+          />
+          <div className='list-item_rankAddress__KZlrz'>
+            <span className=''>0x5ea968...2119</span>
+          </div>
+          <div className='list-item_rankNTFs__3vgEL'>156</div>
+          <div className='list-item_rankHoldValue__2t3sq'>$2,765,380.16</div>
+          <div className='list-item_rankBuyValue__1Fm9x'>$786,378.44</div>
+          <div className='list-item_rankSellValue__2Q65B'>$99,509.78</div>
+          <div className='list-item_rankLastTrade__2OLHH'>
+            <a
+              href='https://etherscan.io/tx/0x5e2b937e791e618962dbf20c47ca6527ddb92a1ecaa67e7f7bc8f80e842a3c8c'
+              target='_blank'
+            >
+              2021-09-29
+            </a>
+          </div>
+        </div>
+        <div className='rank-foot_rankFoot__3sZrD table_rankFoot__H7bSi'>
+          <div className='rank-foot_paginationText__3GrUU'>
+            Showing 1-10 out of 100
+          </div>
+          <div className='rank-pagination_pagination__2o4YT'>
+            <svg
+              width='24'
+              height='24'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+              className='rank-pagination_rankArrow__1KAuw'
+            >
+              <path d='M12 18l-6-6 6-6'></path>
+            </svg>
+            <div className='rank-pagination_rankPaginationNumber__1t1Pc rank-pagination_active__3E9Tk'>
+              1
+            </div>
+            <div className='rank-pagination_rankPaginationNumber__1t1Pc'>2</div>
+            <div className='rank-pagination_rankPaginationNumber__1t1Pc'>3</div>
+            <div className='rank-pagination_rankPaginationNumber__1t1Pc'>4</div>
+            <div className='rank-pagination_rankPaginationNumber__1t1Pc'>5</div>
+            <div className='rank-pagination_rankPaginationNumber__1t1Pc'>
+              ···
+            </div>
+            <div className='rank-pagination_rankPaginationNumber__1t1Pc'>
+              10
+            </div>
+            <svg
+              width='24'
+              height='24'
+              fill='none'
+              xmlns='http://www.w3.org/2000/svg'
+              className='rank-pagination_rankArrowRight__SIVen'
+            >
+              <path d='M12 18l-6-6 6-6'></path>
+            </svg>
+          </div>
+          <div className='rank-foot_paginationSelect__1VkmZ'>
+            <div className='select_selectContainer__1QIph'>
+              <div style={{ width: 'fit-content' }}>
+                <div className='line-number_lineNumber__3ON9J'>
+                  <div className='line-number_row__8Sr9i'>Rows</div>
+                  <span style={{ fontWeight: '500' }}>10</span>
+                  <svg
+                    width='13'
+                    height='5'
+                    viewBox='0 0 13 8'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='line-number_Triangle__G3je5'
+                  >
+                    <path
+                      d='M.579 1.684C-.021 1.045.433 0 1.309 0h10.383c.875 0 1.328 1.045.73 1.684L7.23 7.222a1 1 0 01-1.46 0L.58 1.684z'
+                      fill='#C4C4C4'
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopHolders;
