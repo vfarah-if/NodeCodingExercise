e823928c1738154e0e1b3eceae07abc165f38552
Vincent Farah <vincent.farah@madetech.com>
2022-08-04 08:18:50 +0100

    Minor refactor

--- recipe-ts/src/components/GameOfLife/__tests__/Boards.test.tsx
+++ recipe-ts/src/components/GameOfLife/__tests__/Boards.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import { SimpleBoard } from '../Board/Board.stories';
 
 describe('Board', () => {
-  test('should output a board of simpl items as children', () => {
+  test('should output a board of items as children', () => {
     const { container } = render(<SimpleBoard {...SimpleBoard.args} />);
 
     expect(container).toMatchSnapshot();
