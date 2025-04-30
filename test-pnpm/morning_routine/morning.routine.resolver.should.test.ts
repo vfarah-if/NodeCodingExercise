aef387ca89b5596ec68b20c0834e6303e1702606
Vincent Farah <vincent.farah@gbgplc.com>
2025-04-30 07:35:03 +0100

    Mmake test pass (pass)

--- test-pnpm/morning_routine/morning.routine.resolver.should.test.ts
+++ test-pnpm/morning_routine/morning.routine.resolver.should.test.ts
@@ -2,12 +2,22 @@ import { MorningRoutineResolver } from './morning.routine.resolver';
 
 describe('MorningRoutineResolver should', () => {
   const sut = new MorningRoutineResolver();
+
   test('remind me to do exercise between 06:00 and 6:59', () => {
-    const actual = new Date('2023-10-01T06:30:00Z');
+    const ukTimeBST = '2023-06-01T06:30:00+01:00';
     const expected = 'Do exercise';
 
-    const result = sut.remindMeTo(actual);
+    const result = sut.remindMeTo(new Date(ukTimeBST));
 
     expect(result).toBe(expected);
+  });
+
+  test('remind me not to do exercise between not 06:00 and 6:59', () => {
+    const ukTimeBST = '2023-06-01T05:30:00+01:00';
+    const notExpected = 'Do exercise';
+
+    const result = sut.remindMeTo(new Date(ukTimeBST));
+
+    expect(result).not.toBe(notExpected);
   });
 });
--- test-pnpm/morning_routine/morning.routine.resolver.ts
+++ test-pnpm/morning_routine/morning.routine.resolver.ts
@@ -2,10 +2,15 @@ export class MorningRoutineResolver {
   public remindMeTo(dateTime: Date): string {
     if (this.isTimeToDoExercise(dateTime)) {
       return 'Do exercise';
+    } else {
+      return '';
     }
   }
 
   isTimeToDoExercise(dateTime: Date) {
-    return true;
+    if (dateTime.getHours() === 6 && dateTime.getMinutes() >= 0 && dateTime.getMinutes() <= 59) {
+      return true;
+    }
+    return false;
   }
 }
