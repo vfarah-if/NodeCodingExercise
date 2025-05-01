af83f4c13747c1d8002b5c28bc7251baad00362f
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-01 19:49:02 +0100

    Step3: Make the test just pass

--- test-pnpm/song_birds/song.birds.ts
+++ test-pnpm/song_birds/song.birds.ts
@@ -1,4 +1,5 @@
 export function countSingingSongs(birds: Array<string>): number {
   if (!birds || birds.length === 0) return 0;
   if (birds.length === 1) return 3;
+  if (birds.length === 2) return 18;
 }
