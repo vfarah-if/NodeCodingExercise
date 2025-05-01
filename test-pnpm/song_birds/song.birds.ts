4ad55bc2b86fce66937f992e20e03670e75ca513
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-01 20:32:27 +0100

    Visualised to the max

--- test-pnpm/song_birds/song.birds.ts
+++ test-pnpm/song_birds/song.birds.ts
@@ -1,13 +1,9 @@
 export function countSingingSongs(birds: Array<string>): number {
   if (!birds || birds.length === 0) return 0;
   if (birds.length === 1) {
-    const birdCombos = [['A']];
-    const branchCombos = [
-      ['1', '2'],
-      ['1', '3'],
-      ['2', '3'],
-    ];
-    return birdCombos.length * branchCombos.length * factorial(1);
+    const birdCombos = [['A']]; // 1 bird
+    const branchCombos = [['1'], ['2'], ['3']]; // each possible branch
+    return birdCombos.length * branchCombos.length; // 1 * 3 = 3
   }
   if (birds.length === 2) {
     const birdCombos = [
@@ -22,7 +18,25 @@ export function countSingingSongs(birds: Array<string>): number {
     ];
     return birdCombos.length * branchCombos.length * factorial(2);
   }
-  if (birds.length === 3) return 36;
+  if (birds.length === 3) {
+    const permutationsOfBirds = [
+      ['A', 'B', 'C'],
+      ['A', 'C', 'B'],
+      ['B', 'A', 'C'],
+      ['B', 'C', 'A'],
+      ['C', 'A', 'B'],
+      ['C', 'B', 'A'],
+    ];
+    const permutationsOfBranches = [
+      ['1', '2', '3'],
+      ['1', '3', '2'],
+      ['2', '1', '3'],
+      ['2', '3', '1'],
+      ['3', '1', '2'],
+      ['3', '2', '1'],
+    ];
+    return permutationsOfBirds.length * permutationsOfBranches.length;
+  }
 }
 
 function factorial(n: number): number {
