6f2d4af984ae7b4b98e00b2c26c76d6793334212
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-05 00:34:41 +0100

    Iteration3: Make simple promo pass

--- test-pnpm/shopping-cart-kata/discount.should.test.ts
+++ test-pnpm/shopping-cart-kata/discount.should.test.ts
@@ -1,6 +1,9 @@
+import { Discount } from './discount';
+
 describe('Discount should', () => {
   it('be created from a valid discount code', () => {
     const discount = Discount.fromCode('PROMO_5');
+
     expect(discount.code).toBe('PROMO_5');
     expect(discount.percentage).toBe(0.05);
   });
---
+++ test-pnpm/shopping-cart-kata/discount.ts
@@ -0,0 +1,17 @@
+export class Discount {
+  readonly code: string;
+  readonly percentage: number;
+
+  private constructor(code: string, percentage: number) {
+    this.code = code;
+    this.percentage = percentage;
+  }
+
+  static fromCode(code: string): Discount | null {
+    const map: Record<string, number> = {
+      PROMO_5: 0.05,
+    };
+    const value = map[code];
+    return new Discount(code, value);
+  }
+}
