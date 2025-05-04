75793831cf4c9010e804c068a578f85975999710
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-05 00:55:00 +0100

    Iteration3: Fix the method to apply discounts rounding up

--- test-pnpm/shopping-cart-kata/discount.ts
+++ test-pnpm/shopping-cart-kata/discount.ts
@@ -1,3 +1,5 @@
+import { roundUp } from './utility';
+
 export class Discount {
   readonly code: string;
   readonly percentage: number;
@@ -7,9 +9,16 @@ export class Discount {
     this.percentage = percentage;
   }
 
+  public applyTo(price: number): number {
+    const discountAmount = price * this.percentage;
+    const discountedPrice = price - discountAmount;
+    return roundUp(discountedPrice);
+  }
+
   static fromCode(code: string): Discount | null {
     const map: Record<string, number> = {
       PROMO_5: 0.05,
+      PROMO_10: 0.1,
     };
     const value = map[code];
     return value ? new Discount(code, value) : null;
