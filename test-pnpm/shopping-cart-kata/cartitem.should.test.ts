3046c362231db77d8d00e8eb3ce180021caed818
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-04 14:37:12 +0100

    Iteration2: Make test pass for multiple  items

--- test-pnpm/shopping-cart-kata/cartitem.should.test.ts
+++ test-pnpm/shopping-cart-kata/cartitem.should.test.ts
@@ -4,10 +4,16 @@ import { TaxRate } from './TaxRate';
 
 describe('Adding Cart items should', () => {
   let product = new Product('Iceberg 🥬', 1.55, 0.15, TaxRate.Normal);
+  
   test('calcultate the line total for one item', () => {
     const cartItem = new CartItem(product, 1);
-    const expected = 2.17;
 
-    expect(cartItem.LineTotal).toBe(expected);
+    expect(cartItem.LineTotal).toBe(2.17);
+  });
+
+  test('calcultate the line total for multiple items', () => {
+    const cartItem = new CartItem(product, 3);
+
+    expect(cartItem.LineTotal).toBe(6.51);
   });
 });
