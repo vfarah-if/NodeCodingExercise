294ab3204a01d807fff109fd54826de7b345048a
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-05 00:46:24 +0100

    Iteration3: Failing test for applying a 10% discount

--- test-pnpm/shopping-cart-kata/discount.should.test.ts
+++ test-pnpm/shopping-cart-kata/discount.should.test.ts
@@ -13,4 +13,9 @@ describe('Discount should', () => {
 
     expect(discount).toBeNull();
   });
+
+  it('alculate a discount from a price', () => {
+    const discount = Discount.fromCode('PROMO_10')!;
+    expect(discount.applyTo(100)).toBe(90);
+  });
 });
