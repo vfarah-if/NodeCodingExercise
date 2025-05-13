46cd86097b129d567f6d2833bba09290c0f38005
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-13 10:55:37 +0100

    Phase1: Phase1: Passing test to add a GameService

--- test-pnpm/battleships/command.handler.ts
+++ test-pnpm/battleships/command.handler.ts
@@ -1,10 +1,16 @@
+import { GameService } from './game.service';
+
 export class CommandHandler {
-  constructor(private readonly print: (line: string) => void) {}
+  constructor(
+    private readonly print: (line: string) => void,
+    private readonly gameService: GameService,
+  ) {}
 
   execute(input: string) {
     const [command, ...args] = input.split(/\s+/);
     if (command === 'addPlayer') {
       const name = args.join(' ');
+      this.gameService.addPlayer(name);
       this.print(`Player "${name}" added.`);
     }
   }
---
+++ test-pnpm/battleships/game.service.ts
@@ -0,0 +1,3 @@
+export interface GameService {
+  addPlayer(name: string): void;
+}
