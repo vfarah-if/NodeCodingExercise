c7abf41d9d6dcfa505c284483d800bc19992265c
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-17 13:42:30 +0100

    (GREEN) Refactor test and remove duplication

--- test-pnpm/mars_rovers/mars.rover.should.test.ts
+++ test-pnpm/mars_rovers/mars.rover.should.test.ts
@@ -1,17 +1,19 @@
 import { MarsRover } from './mars.rover';
 
 describe('mars rover should', () => {
-  test('report its default start position', () => {
-    let marsRover = new MarsRover();
+  let marsRover: MarsRover;
 
+  beforeEach(() => {
+    marsRover = new MarsRover();
+  });
+
+  test('report its default start position', () => {
     const actual = marsRover.execute('');
 
     expect(actual).toBe('0:0:N');
   });
 
-  test('move in the same direction with single move command', () => {
-    let marsRover = new MarsRover();
-
+  test('move in the same direction with a single move command', () => {
     const actual = marsRover.execute('M');
 
     expect(actual).toBe('0:1:N');
