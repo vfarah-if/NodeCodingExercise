ee76f413baae123540a79e55d0a7f31945adbbc4
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-17 13:11:50 +0100

    Make simple test pass

--- test-pnpm/mars_rovers/mars.rover.should.test.ts
+++ test-pnpm/mars_rovers/mars.rover.should.test.ts
@@ -2,8 +2,14 @@ describe('mars rover should', () => {
   test('report its default start position', () => {
     let marsRover = new MarsRover();
 
-    const actual = marsRover.Execute('');
+    const actual = marsRover.execute('');
 
     expect(actual).toBe('0:0:N');
   });
 });
+
+export class MarsRover {
+  execute(command: string): string {
+    return '0:0:N';
+  }
+}
