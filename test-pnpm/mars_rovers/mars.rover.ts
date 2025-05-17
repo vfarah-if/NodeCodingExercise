46964fddc7936db0b0319d7abfd077d322f2e280
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-17 15:28:09 +0100

    (GREEN) Implement simple cardinality to bring it back in the correct direction

--- test-pnpm/mars_rovers/mars.rover.ts
+++ test-pnpm/mars_rovers/mars.rover.ts
@@ -1,10 +1,10 @@
 type Direction =
-  | DirectionType.North
-  | DirectionType.East
-  | DirectionType.South
-  | DirectionType.West;
+  | CompassDirection.North
+  | CompassDirection.East
+  | CompassDirection.South
+  | CompassDirection.West;
 
-enum DirectionType {
+enum CompassDirection {
   North = 'N',
   East = 'E',
   South = 'S',
@@ -18,10 +18,10 @@ enum CommandType {
 
 export class MarsRover {
   private _directions: Direction[] = new Array<Direction>(
-    DirectionType.North,
-    DirectionType.East,
-    DirectionType.South,
-    DirectionType.West,
+    CompassDirection.North,
+    CompassDirection.East,
+    CompassDirection.South,
+    CompassDirection.West,
   );
   private _directionIndex = 0;
   private _x: number = 0;
@@ -30,12 +30,22 @@ export class MarsRover {
   execute(commands: string): string {
     for (const command of commands) {
       if (command === CommandType.Move) {
-        this._y = this._y + 1;
+        this.moveForward();
       }
+
       if (command === CommandType.TurnRight) {
-        this._directionIndex = this._directionIndex + 1;
+        this.moveRight();
       }
     }
+
     return `${this._x}:${this._y}:${this._directions[this._directionIndex]}`;
+  }
+
+  private moveRight() {
+    this._directionIndex = (this._directionIndex + 1) % this._directions.length;
+  }
+
+  private moveForward() {
+    this._y = this._y + 1;
   }
 }
