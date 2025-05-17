698086eb86eddf76ac4f82e267aaf74647510cec
Vincent Farah <vincent.farah@gbgplc.com>
2025-05-17 17:27:16 +0100

    (REFACTOR): Group tests so it is easier to understand

--- test-pnpm/mars_rovers/mars.rover.should.test.ts
+++ test-pnpm/mars_rovers/mars.rover.should.test.ts
@@ -9,15 +9,18 @@ describe('mars rover should', () => {
 
   test.each([
     ['report its default start position', '', '0:0:N'],
+
     ['move in the same direction with a single move command', 'M', '0:1:N'],
     ['move forward and then turn facing south', 'MRRM', '0:0:S'],
+    ['move in the same direction with a double move command', 'MM', '0:2:N'],
+    ['move forward facing east', 'RM', '1:0:E'],
+    ['move forward facing west', 'RMLLM', '0:0:W'],
+
     ['turn right facing east', 'R', '0:0:E'],
     ['turn right twice facing south', 'RR', '0:0:S'],
     ['turn right 4 times and face north again', 'RRRR', '0:0:N'],
     ['turn left facing west', 'L', '0:0:W'],
-    ['move in the same direction with a double move command', 'MM', '0:2:N'],
-    ['move forward facing east', 'RM', '1:0:E'],
-    ['move forward facing west', 'RMLLM', '0:0:W'],
+
     ['wrap around north edge to bottom', 'MMMMMMMMMM', '0:0:N'],
     ['wrap around south edge to top', 'RRMMMMMMMMMM', '0:0:S'],
     ['wrap around east edge to the left', 'RMMMMMMMMMM', '0:0:E'],
